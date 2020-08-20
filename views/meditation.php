b5454f8102444b26b6bb6a4d7f77d909d1d5b95c
Jinu <wjdrms735@naver.com>
2020-08-20 17:25:19 +0900

    meditation main section complete

--- views/meditation.php
+++ views/meditation.php
@@ -1,4 +1,28 @@
-<link rel="stylesheet" href="css/meditation">
+<link rel="stylesheet" href="css/meditation.css">
 
 
 <a href="/menu" class="back_btn  flex-center-row"><i class="fas fa-chevron-left"></i></a>
+
+<div class="container">
+    <div class="meditation_main_section">
+        <div class="flex-between-row">
+            <div>
+                <span>만트라 명상서비스</span>
+                <p>양영디지털고등학교</p>
+            </div>
+            <i class="fas fa-heart"></i>
+        </div>
+        <div class="example_box">
+            <div class="example_main_text">
+                <i class="fas fa-microphone"></i>
+                이렇게 말해보세요~
+            </div>
+            <p class="exampl_sub_text"><span>" </span> 만트라 명상 30분 <span> "</span></p>
+            <p class="exampl_sub_text"><span>" </span> 만트라 명상 시작할게 <span> "</span></p>
+        </div>
+    </div>
+
+    <div class="meditation_sub_section">
+        
+    </div>
+</div>
