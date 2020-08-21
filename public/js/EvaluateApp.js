ee8bafce9717a3cd28674d92a915cb469fd09582
ajwwlswotjd <ajwwlswotjd@naver.com>
2020-08-21 10:48:11 +0900

    evaluate top create

--- public/js/EvaluateApp.js
+++ public/js/EvaluateApp.js
@@ -15,7 +15,7 @@ class EvaluateApp {
     render(){
         let txt = `오늘하루를 평가해 점수를 매기고, 한줄평을 작성하는 곳 입니다.`;
         let btnText = "알겠어요!";
-        modal.alert(txt,btnText);
+        // modal.alert(txt,btnText);
     }
 }
 
--- views/evaluate.php
+++ views/evaluate.php
@@ -1,1 +1,14 @@
 <script src="js/EvaluateApp.js"></script>
+<link rel="stylesheet" href="css/evaluate.css">
+
+<div class="evaluate_container">
+    <div class="evaluate_top">
+        <div class="evaluate_top_icons">
+            <i class="fas fa-chevron-left"></i>
+            <span>EVALUATE</span>
+            <i class="far fa-question-circle"></i>
+        </div>
+    </div>
+
+    <div class="evaluate_bottom"></div>
+</div>
