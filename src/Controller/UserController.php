e5289ab5ac227437c83a0e25f3cb39413532d482
ajwwlswotjd <ajwwlswotjd@naver.com>
2020-08-24 11:28:50 +0900

    UserController.php INSERT SQL update

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -34,10 +34,11 @@ class UserController
         $pass = htmlentities($_POST['pass']);
         $date = $_POST['date'];
         $gender = $_POST['gender'];
+        $default_img_path = 'imgs/user_icon.png';
 
-        $sql = "INSERT INTO `mantra_users`(`user_idx`, `user_id`, `user_name` ,`user_password`, `user_birthday`, `user_gender`)
-         VALUES (null , ? , ? , PASSWORD(?) , ? , ?)";
-        $result = DB::execute($sql, [$id, $name, $pass, $date, $gender]);
+        $sql = "INSERT INTO `mantra_users`(`user_idx`, `user_id`, `user_name` ,`user_password`, `user_birthday`, `user_gender` , `user_img`)
+         VALUES (null , ? , ? , PASSWORD(?) , ? , ? , ?)";
+        $result = DB::execute($sql, [$id, $name, $pass, $date, $gender,$default_img_path]);
         Library::sendJson(["register" => $result]);
     }
 
